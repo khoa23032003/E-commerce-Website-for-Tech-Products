70d952ba364fef9367a89719d76fbdaec5410b2f
khoa23032003 <3minhkhoa23032003@gmail.com>
2024-10-23 09:11:48 +0700

    update layout

--- src/app/users/HomePage/page.tsx
+++ src/app/users/HomePage/page.tsx
@@ -1,31 +1,128 @@
 // src/app/users/Page.tsx
+import Image from "next/image";
 import React from "react";
 
 const Page = () => {
   return (
     <div className="w-full">
       {/* BANNER */}
-      <div>
+      <div className="px-4">
         {/* BANNER-TOP */}
         <div className="flex">
           {/* BANNER-TOP-LEFT */}
-          <div className="w-2/3 bg-black h-[450px]"></div>
+          <div className="w-3/4">
+            {/* IMAGE-BIG */}
+            <div className="  h-[250px] p-2">
+              <img
+                src="/Bannertop1.jpg"
+                className="w-full h-full rounded-lg "
+              />
+            </div>
+            {/* IMaGE-ITEM */}
+            <div className="flex">
+              <div className="w-1/3  h-[125px] p-2">
+                <img
+                  src="/Bannertop1.jpg"
+                  className="w-full h-full rounded-lg "
+                />
+              </div>
+              <div className="w-1/3  h-[125px] p-2">
+                <img
+                  src="/Bannertop1.jpg"
+                  className="w-full h-full rounded-lg "
+                />
+              </div>
+              <div className="w-1/3  h-[125px] p-2">
+                <img
+                  src="/Bannertop1.jpg"
+                  className="w-full h-full rounded-lg "
+                />
+              </div>
+            </div>
+          </div>
           {/* BANNER-TOP-RIGHT */}
-          <div className="w-1/3 bg-purple-300 h-[450px]"></div>
+          <div className="w-1/4">
+            <div className="  h-[250px] p-2">
+              <img
+                src="/Bannertop1.jpg"
+                className="w-full h-full rounded-lg "
+              />
+            </div>
+            <div className=" h-[125px] p-2">
+              <img
+                src="/Bannertop1.jpg"
+                className="w-full h-full rounded-lg "
+              />
+            </div>
+          </div>
         </div>
         {/* BANNER-BOTTOM */}
         <div className="flex"></div>
       </div>
-      <div>
-        {/* BANNER-TOP */}
-        <div className="flex">
-          {/* BANNER-TOP-LEFT */}
-          <div className="w-2/3 bg-black h-[450px]"></div>
-          {/* BANNER-TOP-RIGHT */}
-          <div className="w-1/3 bg-purple-300 h-[450px]"></div>
+
+      {/* CONTENT */}
+      <div className="flex w-full">
+        <div className="w-1/5 bg-blue-400 h-full mx-2">
+          <img src="/bannerconten.jpg" alt="" className="h-[150px] w-full" />
+
+          <div className="w-1/5 bg-blue-400 h-full r  shadow-lg w-full">
+            <div className="text-[10px] font-bold text-white mb-2 flex flex-col items-start">
+              Danh mục
+              <span className="w-full h-[2px] bg-white mt-1"></span>
+            </div>
+
+            <ul className="text-[8px] text-gray-800 mx-2">
+              <li className="mb-1">
+                <a
+                  href=""
+                  className="hover:text-blue-600 transition duration-200"
+                >
+                  Màn hình
+                </a>
+              </li>
+              <li className="mb-1">
+                <a
+                  href=""
+                  className="hover:text-blue-600 transition duration-200"
+                >
+                  Máy tính
+                </a>
+              </li>
+              <li className="mb-1">
+                <a
+                  href=""
+                  className="hover:text-blue-600 transition duration-200"
+                >
+                  PC
+                </a>
+              </li>
+              <li className="mb-1">
+                <a
+                  href=""
+                  className="hover:text-blue-600 transition duration-200"
+                >
+                  PC
+                </a>
+              </li>
+              <li className="mb-1">
+                <a
+                  href=""
+                  className="hover:text-blue-600 transition duration-200"
+                >
+                  PC
+                </a>
+              </li>
+              <li>
+                <a
+                  href=""
+                  className="hover:text-blue-600 transition duration-200"
+                >
+                  PC
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        {/* BANNER-BOTTOM */}
-        <div className="flex"></div>
       </div>
     </div>
   );
--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -10,10 +10,10 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="fixed top-0 left-0 right-0 z-10">
+      <header className="fixed top-0 left-0 right-0 z-10 ">
         <Navbar />
       </header>
-      <main className="flex-grow pt-16 pb-16 overflow-auto">
+      <main className="flex-grow pt-16 pb-16 overflow-auto p=0 m-0">
         {" "}
         {/* Đệm cho header và footer */}
         {children}
