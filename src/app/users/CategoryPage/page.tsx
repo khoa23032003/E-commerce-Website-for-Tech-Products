119d6d894da79d7b7d8ca72a0bd2c0239ad93315
Tuan <dangvohoangtuan0404@gmail.com>
2024-10-23 09:51:29 +0700

    fix conflig

--- src/app/users/CategoryPage/page.tsx
+++ src/app/users/CategoryPage/page.tsx
@@ -34,6 +34,7 @@ const Page = () => {
                 <CardComponent />
                 <CardComponent />
                 <CardComponent />
+                <CardComponent />
             </div>
 
 
